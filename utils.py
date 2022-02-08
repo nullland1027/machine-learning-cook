2cb4b3475a4807d17cee993b6e03e22f0866346a
Jason Bourne <rustylake2000@qq.com>
2022-02-08 15:52:10 +0800

    添加util工具函数文件

---
+++ utils.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def plot_classifier(classifier, X, y):
+    # 定义图形取值范围
+    x_min, x_max = min(X[:, 0]) - 1, max(X[:, 0]) + 1
+    y_min, y_max = min(X[:, 1]) - 1, max(X[:, 1]) + 1
+
+    # 设置step
+    step_size = 0.01
+    # 定义网格
+    x_values, y_values = np.meshgrid(np.arange(x_min, x_max, step_size),
+                                     np.arange(y_min, y_max, step_size))
+    mesh_output = classifier.predict(np.c_[x_values.ravel(), y_values.ravel()])  # 计算分类器输出结果
+    mesh_output = mesh_output.reshape(x_values.shape)  # 数组维度变形
+
+    # 画出分类结果
+    plt.figure()
+    plt.pcolormesh(x_values, y_values, mesh_output, cmap=plt.cm.gray)
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=80, edgecolors='black', linewidths=1, cmap=plt.cm.Paired)
+
+    # 图形取值范围
+    plt.xlim(x_values.min(), x_values.max())
+    plt.ylim(y_values.min(), y_values.max())
+
+    # 设置x，y轴
+    plt.xticks((np.arange(int(min(X[:, 0]) - 1), int(max(X[:, 0]) + 1), 1.0)))
+    plt.xticks((np.arange(int(min(X[:, 1]) - 1), int(max(X[:, 1]) + 1), 1.0)))
+
+    plt.show()
